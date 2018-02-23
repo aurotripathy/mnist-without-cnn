12732454ec5b329fef3b9ffb08854f184c4ba7ef
Auro Tripathy <ipserv@yahoo.com>
2018-02-22 22:46:03 -0800

    Initial

--- mnist_classify_w_zernike.py
+++ mnist_classify_w_zernike.py
@@ -21,21 +21,17 @@ print('size x_test', x_test.shape)
 print('Number of classes', nb_classes)
 print('Shape of image', x_train[0].shape, 'Type of image', type(x_train[0]))
 
-
 zm = ZernikeMoments(x_train[0].shape[0])
 x_train_zm = np.asarray([zm.describe(x) for x in x_train])
 x_test_zm = np.asarray([zm.describe(x) for x in x_test])
-print('Shape of descrptors', x_train_zm.shape, 'Type of descriptor', type(x_train_zm))
-print('Shape of descrptors', x_test_zm.shape, 'Type of descriptor', type(x_test_zm))
-
+print('Shape of descriptors', x_train_zm.shape, 'Type of descriptor', type(x_train_zm))
+print('Shape of descriptors', x_test_zm.shape, 'Type of descriptor', type(x_test_zm))
 
 descriptor_shape = x_train_zm[0].shape
 print('Input shape of descripor is:', descriptor_shape)
 
-
 y_train_binary = to_categorical(y_train, nb_classes)
 y_test_binary = to_categorical(y_test, nb_classes)
-
 
 model = Sequential()
 model.add(Dense(100, input_shape=descriptor_shape, activation='relu'))
@@ -43,10 +39,14 @@ Dropout(0.5)
 model.add(Dense(100, input_shape=descriptor_shape, activation='relu'))
 Dropout(0.5)
 model.add(Dense(nb_classes, activation='softmax'))
+print(model.summary())
 
 model = multi_gpu_model(model, gpus=3)
 
-print(model.summary())
-
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train_zm, y_train_binary, batch_size=128, epochs=500, validation_data=(x_test_zm, y_test_binary))
+model.compile(optimizer='Adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+model.fit(x_train_zm, y_train_binary, 
+          batch_size=128, 
+          epochs=500, 
+          validation_data=(x_test_zm, y_test_binary))
